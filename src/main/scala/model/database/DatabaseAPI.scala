51be62d27ff6d368d6e4600e1e1a3bedfafa4f3b
jessehartloff <hartloff@buffalo.edu>
2020-04-23 16:40:18 -0400

    add developer mode -Developer mode does not require a MySQL database connection

---
+++ src/main/scala/model/database/DatabaseAPI.scala
@@ -0,0 +1,11 @@
+package model.database
+
+import model.StudentInQueue
+
+trait DatabaseAPI {
+
+  def addStudentToQueue(student: StudentInQueue): Unit
+  def removeStudentFromQueue(username: String): Unit
+  def getQueue: List[StudentInQueue]
+
+}
