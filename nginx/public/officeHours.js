e25cdc0da72e69262c3fbafbfbf76046e3e91a42
Nil <nildham21@gmail.com>
2020-04-25 00:42:25 -0400

    ~Adding Student side function that will load the html that the student will see

--- nginx/public/officeHours.js
+++ nginx/public/officeHours.js
@@ -3,6 +3,17 @@ const socket = io.connect("http://localhost:8080", {transports: ['websocket']});
 socket.on('queue', displayQueue);
 socket.on('message', displayMessage);
 
+let lecture=Array("Unit Testing","Test Factoring","Voting Class","Reference Referee","Reference Trader","Reference Batteries",
+    "Inheritance Batteries","Polymorphic Electronics","JSON Store","Player States","TV States","Car States",
+    "Function and Type Parameters","Recursive Fibonacci","Recursive Factoring","Average in Range","Immutable Point",
+    "Linked-List Reduce","Backlog","Expression Trees","BST toList","Graph Connections","Graph Distance",
+    "Actors","Banking Actor","Traffic Actors","Websocket","Echo Server","DM Server")
+
+let hw=Array("Physics Engine","Rhyming Dictionary","Calculator","Microwave","Genetic Algorithm","Recommendations","Decision Tree","Maze Solver","Clicker")
+let appObj=Array("Program Execution","Object-Oriented Programming","Functional Programming","Data Structures & Algorithms","Event-Based Architectures")
+let lab=Array("Program Execution","Object-Oriented Programming","Functional Programming","Data Structures & Algorithms","Event-Based Architectures")
+
+
 function displayMessage(newMessage) {
     document.getElementById("message").innerHTML = newMessage;
 }
@@ -14,6 +25,14 @@ function displayQueue(queueJSON) {
         formattedQueue += student['username'] + " has been waiting since " + student['timestamp'] + "<br/>"
     }
     document.getElementById("queue").innerHTML = formattedQueue;
+}
+
+function studentSide() {
+    document.getElementById("test").innerHTML=""
+    document.getElementById("subtile").innerHTML="Select what you need assistance in"
+
+    let formatSelections=""
+
 }
 
 
