b5bfb750d86172cb314fa59a9bb39cff2cdb2419
matth <matth4497@gmail.com>
2020-04-26 04:06:19 -0400

    Must specify if you are a TA or a student after entering name TA's are now the only ones who can see the queue and the ready to help button, students only receive their position in the queue Once a TA presses the ready to help button, the student who has been waiting the longest receives a message with the username of the TA who is ready to help them

--- nginx/public/officeHours.js
+++ nginx/public/officeHours.js
@@ -1,10 +1,23 @@
 const socket = io.connect("http://localhost:8080", {transports: ['websocket']});
 
 socket.on('queue', displayQueue);
-socket.on('message', displayMessage);
+socket.on('studentMessage', displayStudentMessage);
+socket.on('taMessage',displayTaMessage);
+socket.on('position', displayPosition);
 
-function displayMessage(newMessage) {
-    document.getElementById("message").innerHTML = newMessage;
+function displayStudentMessage(newMessage) {
+    document.getElementById("initial").innerHTML = newMessage;
+    document.getElementById("queue").innerHTML = '';
+}
+
+function displayTaMessage(newMessage) {
+    document.getElementById("messages").innerHTML = newMessage;
+}
+
+
+function displayPosition(position){
+    let info = JSON.parse(position);
+    document.getElementById("queue").innerHTML = "Queue Position " + info.toString();
 }
 
 function displayQueue(queueJSON) {
@@ -17,10 +30,19 @@ function displayQueue(queueJSON) {
 }
 
 
-function enterQueue() {
+function enterQueueTA() {
     let name = document.getElementById("name").value;
-    socket.emit("enter_queue", name);
-    document.getElementById("name").value = "";
+    socket.emit("enter_queue_TA", name);
+    let html = '<button onclick="readyToHelp();">TA Ready to Help</button>';
+    document.getElementById("initial").innerHTML = html;
+    document.getElementById("messages").innerHTML = '';
+}
+
+function enterQueueStudent() {
+    let name = document.getElementById("name").value;
+    socket.emit("enter_queue_Student", name);
+    let html = "<h1>Waiting for next Available TA </h1>";
+    document.getElementById("initial").innerHTML = html
 }
 
 function readyToHelp() {
--- src/main/scala/model/OfficeHoursServer.scala
+++ src/main/scala/model/OfficeHoursServer.scala
@@ -8,14 +8,17 @@ import play.api.libs.json.{JsValue, Json}
 
 class OfficeHoursServer() {
 
+
   val database: DatabaseAPI = if(Configuration.DEV_MODE){
     new TestingDatabase
   }else{
     new Database
   }
 
+  var peopleWaiting:List[String] = List()
   var usernameToSocket: Map[String, SocketIOClient] = Map()
   var socketToUsername: Map[SocketIOClient, String] = Map()
+  var taSocketToUsername:Map[SocketIOClient,String] = Map()
 
   val config: Configuration = new Configuration {
     setHostname("0.0.0.0")
@@ -25,7 +28,8 @@ class OfficeHoursServer() {
   val server: SocketIOServer = new SocketIOServer(config)
 
   server.addDisconnectListener(new DisconnectionListener(this))
-  server.addEventListener("enter_queue", classOf[String], new EnterQueueListener(this))
+  server.addEventListener("enter_queue_TA", classOf[String], new EnterQueueTAListener(this))
+  server.addEventListener("enter_queue_Student", classOf[String], new EnterQueueStudentListener(this))
   server.addEventListener("ready_for_student", classOf[Nothing], new ReadyForStudentListener(this))
 
   server.start()
@@ -54,16 +58,35 @@ class DisconnectionListener(server: OfficeHoursServer) extends DisconnectListene
         server.usernameToSocket -= username
       }
     }
+
+    if(server.taSocketToUsername.contains(socket)){
+        server.taSocketToUsername -= socket
+    }
   }
 }
 
 
-class EnterQueueListener(server: OfficeHoursServer) extends DataListener[String] {
+class EnterQueueTAListener(server: OfficeHoursServer) extends DataListener[String] {
   override def onData(socket: SocketIOClient, username: String, ackRequest: AckRequest): Unit = {
-    server.database.addStudentToQueue(StudentInQueue(username, System.nanoTime()))
     server.socketToUsername += (socket -> username)
     server.usernameToSocket += (username -> socket)
-    server.server.getBroadcastOperations.sendEvent("queue", server.queueJSON())
+    server.taSocketToUsername += (socket -> username)
+    socket.sendEvent("queue", server.queueJSON())
+  }
+}
+
+class EnterQueueStudentListener(server: OfficeHoursServer) extends DataListener[String] {
+  override def onData(socket: SocketIOClient, username: String, ackRequest: AckRequest): Unit = {
+    server.database.addStudentToQueue(StudentInQueue(username, System.nanoTime()))
+    server.peopleWaiting = server.peopleWaiting :+ username
+    server.socketToUsername += (socket -> username)
+    server.usernameToSocket += (username -> socket)
+    socket.sendEvent("position", Json.stringify(Json.toJson(server.peopleWaiting.length)))
+
+    for(x <- server.taSocketToUsername.keys){
+      x.sendEvent("queue",server.queueJSON())
+    }
+
   }
 }
 
@@ -72,13 +95,23 @@ class ReadyForStudentListener(server: OfficeHoursServer) extends DataListener[No
   override def onData(socket: SocketIOClient, dirtyMessage: Nothing, ackRequest: AckRequest): Unit = {
     val queue = server.database.getQueue.sortBy(_.timestamp)
     if(queue.nonEmpty){
+      server.peopleWaiting = server.peopleWaiting.drop(1)
       val studentToHelp = queue.head
       server.database.removeStudentFromQueue(studentToHelp.username)
-      socket.sendEvent("message", "You are now helping " + studentToHelp.username)
-      if(server.usernameToSocket.contains(studentToHelp.username)){
-        server.usernameToSocket(studentToHelp.username).sendEvent("message", "A TA is ready to help you")
+      socket.sendEvent("taMessage", "You are now helping " + studentToHelp.username)
+
+      val message:String = "TA " + server.taSocketToUsername(socket) + " is ready to help you!"
+      server.usernameToSocket(studentToHelp.username).sendEvent("studentMessage", message)
+
+      for(x <- server.taSocketToUsername.keys){
+        x.sendEvent("queue",server.queueJSON())
       }
-      server.server.getBroadcastOperations.sendEvent("queue", server.queueJSON())
+
+
+      for(i <- server.peopleWaiting){
+        server.usernameToSocket(i).sendEvent("position", Json.stringify(Json.toJson(server.peopleWaiting.indexOf(i) + 1)))
+      }
+
     }
   }
 }
