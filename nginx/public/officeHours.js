efe2142ff6d2e01fc788f100b60ceaeff11fc89a
dashen1228 <dashen1228@gmail.com>
2020-04-29 23:47:58 -0400

    Be able to distinguish between students and TAs now Implemented a simple authentication for TAs Now only TAs can see the full queue Now students can only see their own positions Now students could not create multiple usernames and use them to enter the queue under the same socket(website).

--- nginx/public/officeHours.js
+++ nginx/public/officeHours.js
@@ -3,6 +3,15 @@ const socket = io.connect("http://localhost:8080", {transports: ['websocket']});
 socket.on('queue', displayQueue);
 socket.on('message', displayMessage);
 
+//listen 'position' to display each student's position
+socket.on('position', displayPosition);
+
+//listen 'boolean' to check if the TA code is correct or not
+socket.on('boolean', Action);
+
+//Assume all clients at the beginning are not the TA
+var TAstate = false;
+
 function displayMessage(newMessage) {
     document.getElementById("message").innerHTML = newMessage;
 }
@@ -16,6 +25,11 @@ function displayQueue(queueJSON) {
     document.getElementById("queue").innerHTML = formattedQueue;
 }
 
+//display student's position in html
+function displayPosition(YourPosition){
+    document.getElementById("position").innerHTML = YourPosition;
+}
+
 
 function enterQueue() {
     let name = document.getElementById("name").value;
@@ -23,6 +37,34 @@ function enterQueue() {
     document.getElementById("name").value = "";
 }
 
-function readyToHelp() {
-    socket.emit("ready_for_student");
+//grab the code from html to check if it's the right code, send this from the client to the server
+function checkCode() {
+    let code = document.getElementById("TAcode").value;
+    socket.emit("validate", code);
 }
+
+//base on the verification, give different actions using control flow
+function Action(boolean){
+    if (boolean == "true" && TAstate == false){
+        TAstate = true;
+        var help = document.createElement("button");
+        help.innerHTML = "TA Ready to Help";
+        document.body.appendChild(help);
+        help.addEventListener ("click", function readyToHelp(){
+            socket.emit("ready_for_student");
+        });
+        alert("valid code");
+        socket.emit("verified")
+    }
+    else if (boolean == "true" && TAstate == true){
+        alert("you are TA already!");
+    }
+    else{
+        alert("invalid code");
+    }
+}
+
+//abandon this
+//function readyToHelp() {
+    //socket.emit("ready_for_student");
+//}
--- src/main/scala/model/OfficeHoursServer.scala
+++ src/main/scala/model/OfficeHoursServer.scala
@@ -17,6 +17,10 @@ class OfficeHoursServer() {
   var usernameToSocket: Map[String, SocketIOClient] = Map()
   var socketToUsername: Map[SocketIOClient, String] = Map()
 
+  //distinguish between students and TAs using data structures, so I can let them see the whole queue/their position.
+  var TAsocketList: List[SocketIOClient] = List()
+  var StudentSocketList: List[SocketIOClient] = List()
+
   val config: Configuration = new Configuration {
     setHostname("0.0.0.0")
     setPort(8080)
@@ -27,6 +31,12 @@ class OfficeHoursServer() {
   server.addDisconnectListener(new DisconnectionListener(this))
   server.addEventListener("enter_queue", classOf[String], new EnterQueueListener(this))
   server.addEventListener("ready_for_student", classOf[Nothing], new ReadyForStudentListener(this))
+
+  //listen for the TA code and check
+  server.addEventListener("validate", classOf[String], new Validation(this))
+
+  //if you have a valid TA code, your socket will be added to the TA list
+  server.addEventListener("verified", classOf[Nothing], new AddTA(this))
 
   server.start()
 
@@ -60,10 +70,25 @@ class DisconnectionListener(server: OfficeHoursServer) extends DisconnectListene
 
 class EnterQueueListener(server: OfficeHoursServer) extends DataListener[String] {
   override def onData(socket: SocketIOClient, username: String, ackRequest: AckRequest): Unit = {
-    server.database.addStudentToQueue(StudentInQueue(username, System.nanoTime()))
+
+    //check if the student socket list already contained that socket, this is to avoid a single student enter queue repeatly
+    //also, all of the usernames created under the same socket will also be ignored
+    //however, this functionality will be limited because one single student could create many websites which means many web clients.
+
+    if(!server.StudentSocketList.contains(socket)) {
+      server.StudentSocketList = server.StudentSocketList :+ socket
+      server.database.addStudentToQueue(StudentInQueue(username, System.nanoTime()))
+    }
     server.socketToUsername += (socket -> username)
     server.usernameToSocket += (username -> socket)
-    server.server.getBroadcastOperations.sendEvent("queue", server.queueJSON())
+
+    //display the whole updated queue to all of the TAs
+    for(ta <- server.TAsocketList) {
+      ta.sendEvent("queue", server.queueJSON())
+    }
+
+    //display the each student's position, students can't have access to see the whole queue except their position
+    socket.sendEvent("position", "You" + "(" + username + ")" + "are now in position" + server.StudentSocketList.indexOf(socket).toString)
   }
 }
 
@@ -78,8 +103,44 @@ class ReadyForStudentListener(server: OfficeHoursServer) extends DataListener[No
       if(server.usernameToSocket.contains(studentToHelp.username)){
         server.usernameToSocket(studentToHelp.username).sendEvent("message", "A TA is ready to help you")
       }
-      server.server.getBroadcastOperations.sendEvent("queue", server.queueJSON())
+
+      //display the updated queue to all the TAs again
+      for(ta <- server.TAsocketList) {
+        ta.sendEvent("queue", server.queueJSON())
+      }
+
+      //avoid head of empty list error, and display each student's updated position
+      if(server.StudentSocketList.nonEmpty) {
+        val head = server.StudentSocketList.head
+        head.sendEvent("position", "It's your position now!")
+
+        //remove the head of the queue
+        server.StudentSocketList = server.StudentSocketList.tail
+        for (student <- server.StudentSocketList) {
+          student.sendEvent("position", "You" + "(" + server.socketToUsername(student) + ")" + "are now in position" + server.StudentSocketList.indexOf(student).toString)
+        }
+      }
     }
+  }
+}
+
+//if the code is 20001228, TA verified, else not
+class Validation(server: OfficeHoursServer) extends DataListener[String] {
+  override def onData(socket: SocketIOClient, code: String, ackRequest: AckRequest): Unit = {
+    if(code == "20001228"){
+      socket.sendEvent("boolean", "true")
+    }
+    else{
+      socket.sendEvent("boolean", "false")
+    }
+  }
+}
+
+//verified TA sockets will be added to the TA list.
+class AddTA(server: OfficeHoursServer) extends DataListener[Nothing] {
+  override def onData(socket: SocketIOClient, meaningless: Nothing, ackRequest: AckRequest): Unit = {
+    server.TAsocketList = server.TAsocketList :+ socket
+    socket.sendEvent("queue", server.queueJSON())
   }
 }
 
