641d8be32f9d58ed75a1804abf24280649c22185
ArlieFuentson <57299645+ArlieFuentson@users.noreply.github.com>
2020-05-05 17:09:14 -0400

    Lots of changes: 1) index.html UI 2) officeHours.js * Added functionality for new buttons and sending messages to the server 3) Server * PasswordCheck for TA's * Students cannot remove students from the queue without entering the Password * Greater information shown (TA can see what student they are helping, and vice versa) * More classes in the Server to account for functionality * TA password stored in the server (should be changed if deployed). * Changed the way Users and Sockets are stored (created UniqueUser). --> updated existing classes

--- nginx/public/officeHours.js
+++ nginx/public/officeHours.js
@@ -17,13 +17,69 @@ function displayQueue(queueJSON) {
 }
 
 
-function enterQueue() {
-    let name = document.getElementById("name").value;
-    socket.emit("enter_queue", name);
-    document.getElementById("name").value = "";
-    //document.getElementById("queue").hidden = true
-}
+// function enterQueue() {
+//     //let name = document.getElementById("name").value;
+//     socket.emit("enter_queue", name);
+//     document.getElementById("name").value = "";
+// }
 
 function readyToHelp() {
     socket.emit("ready_for_student");
 }
+
+
+//NEW! --> Later implement functionality to deal with empty names? Simple conditional
+function registerStudent() {
+    let name = document.getElementById("name").value;
+    socket.emit("student", name);
+    document.getElementById("name").value = "";
+
+    document.getElementById("name").hidden = true;
+    document.getElementById("header").innerHTML = "The queue is:";
+    document.getElementById("header2").hidden = true;
+    document.getElementById("student_register").hidden = true;
+    document.getElementById("ta_register").hidden = true
+}
+
+function registerTA() {
+    let name = document.getElementById("name").value;
+    socket.emit("ta", name);
+    document.getElementById("name").value = "";
+    document.getElementById("header").innerHTML = "Enter the password:";
+    // Remember the difference between .value and .innerHTML !
+
+    //document.getElementById("name").hidden = true
+    //document.getElementById("header").hidden = true
+    document.getElementById("header2").hidden = true;
+    document.getElementById("student_register").hidden = true;
+    document.getElementById("ta_register").hidden = true;
+
+    document.getElementById("password_submit").hidden = false;
+}
+
+//Obviously, this should be more secure, but the idea is here. At least I'm not storing the password in the front end!
+function checkPassword(){
+    let password = document.getElementById("name").value;
+    socket.emit("password_check", password);
+    document.getElementById("name").value = "";
+
+    document.getElementById("ready_to_help_button").hidden = false;
+}
+
+// socket.on('verified', displayReadyButton);
+// function displayReadyButton(){
+//     //document.getElementById("ready_to_help_button").hidden = false;
+
+
+// socket.on('connect', onConnect);
+// function onConnect() {
+//     //Hide all unecessary elements for now!
+//     //document.getElementById("password_submit").hidden = true;
+//
+// }
+
+//Security implemented because I will have the Backend checking each time someone hits the button for "remove from list".
+//This shouldn't be visible on a students screen, but even if they adjust HTML, they won't be able to click it and have functionality happen
+//Because the server will double check they are actually a TA!
+
+//Add functionality so that if a TA clicks on a student, they are told their TA!
--- src/main/scala/model/OfficeHoursServer.scala
+++ src/main/scala/model/OfficeHoursServer.scala
@@ -14,13 +14,17 @@ class OfficeHoursServer() {
     new Database
   }
 
-  var usernameToSocket: Map[String, SocketIOClient] = Map()
-  var socketToUsername: Map[SocketIOClient, String] = Map()
+  var users: List[UniqueUser] = List()
+//  var usernameToSocket: Map[String, SocketIOClient] = Map()
+//  var socketToUsername: Map[SocketIOClient, String] = Map()
 
   val config: Configuration = new Configuration {
     setHostname("0.0.0.0")
     setPort(8080)
   }
+
+  //todo Change password for TA's here! (Obviously one shouldn't store a password here when deploying)!
+  val passwordTA: String = "apple"
 
   val server: SocketIOServer = new SocketIOServer(config)
 
@@ -29,11 +33,16 @@ class OfficeHoursServer() {
   //If they are a student, they can only see THEIR request to be helped.
   //If they are a TA, enter a password (for now will just check a string), and then they can see the whole queue.
 
-  server.addConnectListener(new ConnectionListener(this))
+//  server.addConnectListener(new ConnectionListener(this))
 
   server.addDisconnectListener(new DisconnectionListener(this))
-  server.addEventListener("enter_queue", classOf[String], new EnterQueueListener(this))
+//  server.addEventListener("enter_queue", classOf[String], new EnterQueueListener(this))
   server.addEventListener("ready_for_student", classOf[Nothing], new ReadyForStudentListener(this))
+
+  server.addEventListener("student", classOf[String], new Student(this))
+  server.addEventListener("ta", classOf[String], new TA(this))
+  server.addEventListener("password_check", classOf[String], new PasswordCheck(this))
+
 
   server.start()
 
@@ -51,46 +60,112 @@ object OfficeHoursServer {
   }
 }
 
-class ConnectionListener(server: OfficeHoursServer) extends ConnectListener {
-  override def onConnect(socketIOClient: SocketIOClient): Unit = {
+//New -- might not need
+//class ConnectionListener(server: OfficeHoursServer) extends ConnectListener {
+//  override def onConnect(socketIOClient: SocketIOClient): Unit = {
+//    //send a message to the Client to ONLY DISPLAY THE TWO BUTTONS !!
+//  }
+//}
 
+class Student(server: OfficeHoursServer) extends DataListener[String] {
+  override def onData(socket: SocketIOClient, username: String, ackRequest: AckRequest): Unit = {
+    server.users = new UniqueUser(username, socket, false) :: server.users
+    //Send them the queue/enter them right away
+    //Got rid of "EnterQueueListener"
+    server.database.addStudentToQueue(StudentInQueue(username, System.nanoTime()))
+    server.server.getBroadcastOperations.sendEvent("queue", server.queueJSON())
   }
 }
 
-class DisconnectionListener(server: OfficeHoursServer) extends DisconnectListener {
-  override def onDisconnect(socket: SocketIOClient): Unit = {
-    if (server.socketToUsername.contains(socket)) {
-      val username = server.socketToUsername(socket)
-        server.socketToUsername -= socket
-      if (server.usernameToSocket.contains(username)) {
-        server.usernameToSocket -= username
+class TA(server: OfficeHoursServer) extends DataListener[String] {
+  override def onData(socket: SocketIOClient, username: String, ackRequest: AckRequest): Unit = {
+    server.users = new UniqueUser(username, socket, false) :: server.users
+    //Don't send the queue until they've been verified as a TA
+    server.server.getBroadcastOperations.sendEvent("queue", server.queueJSON())
+  }
+}
+
+class PasswordCheck(server: OfficeHoursServer) extends DataListener[String] {
+  override def onData(socket: SocketIOClient, input: String, ackRequest: AckRequest): Unit = {
+    if (input==server.passwordTA){
+      for (i <- server.users){
+        if (i.getSocket==socket){
+          i.ta = true
+          // Send the Client a response to setup the HTML correctly!
+          //server.server.getBroadcastOperations.sendEvent("verified")
+
+        }
       }
     }
   }
 }
 
-
-class EnterQueueListener(server: OfficeHoursServer) extends DataListener[String] {
-  override def onData(socket: SocketIOClient, username: String, ackRequest: AckRequest): Unit = {
-    server.database.addStudentToQueue(StudentInQueue(username, System.nanoTime()))
-    server.socketToUsername += (socket -> username)
-    server.usernameToSocket += (username -> socket)
-    server.server.getBroadcastOperations.sendEvent("queue", server.queueJSON())
+//Used a similar data structure in my Clicker (took out the Actor references)
+class UniqueUser(username: String, socket: SocketIOClient, var ta: Boolean){
+  def getUsername: String ={
+    this.username
+  }
+  def getSocket: SocketIOClient ={
+    this.socket
+  }
+  def getTa: Boolean ={
+    this.ta
   }
 }
 
 
+class DisconnectionListener(server: OfficeHoursServer) extends DisconnectListener {
+  override def onDisconnect(socket: SocketIOClient): Unit = {
+    server.users = server.users.filter(_.getSocket != socket) //Effectively 'removes' the user who disconnected
+//    if (server.socketToUsername.contains(socket)) {
+//      val username = server.socketToUsername(socket)
+//        server.socketToUsername -= socket
+//      if (server.usernameToSocket.contains(username)) {
+//        server.usernameToSocket -= username
+//      }
+//    }
+  }
+}
+
+//class EnterQueueListener(server: OfficeHoursServer) extends DataListener[String] {
+//  override def onData(socket: SocketIOClient, username: String, ackRequest: AckRequest): Unit = {
+//    server.database.addStudentToQueue(StudentInQueue(username, System.nanoTime()))
+//    server.socketToUsername += (socket -> username)
+//    server.usernameToSocket += (username -> socket)
+//    server.server.getBroadcastOperations.sendEvent("queue", server.queueJSON())
+//  }
+//}
+
 class ReadyForStudentListener(server: OfficeHoursServer) extends DataListener[Nothing] {
   override def onData(socket: SocketIOClient, dirtyMessage: Nothing, ackRequest: AckRequest): Unit = {
-    val queue = server.database.getQueue.sortBy(_.timestamp)
-    if(queue.nonEmpty){
-      val studentToHelp = queue.head
-      server.database.removeStudentFromQueue(studentToHelp.username)
-      socket.sendEvent("message", "You are now helping " + studentToHelp.username)
-      if(server.usernameToSocket.contains(studentToHelp.username)){
-        server.usernameToSocket(studentToHelp.username).sendEvent("message", "A TA is ready to help you")
+    //first need to check if the socket is a TA!
+    for (i <- server.users) {
+      if (i.getSocket == socket && i.getTa) {
+
+        val queue = server.database.getQueue.sortBy(_.timestamp)
+        if (queue.nonEmpty) {
+          val studentToHelp = queue.head
+          server.database.removeStudentFromQueue(studentToHelp.username)
+
+          for (loop <- server.users) {
+            if (loop.getUsername == studentToHelp.username) {
+              loop.getSocket.sendEvent("message", i.getUsername + " is ready to help you!")
+            }
+          }
+
+          socket.sendEvent("message", "You are now helping " + studentToHelp.username + "!")
+          //socket.sendEvent("message", taName + " is now helping you")
+          //          if (server.usernameToSocket.contains(studentToHelp.username)) {
+          //            server.usernameToSocket(studentToHelp.username).sendEvent("message", "A TA is ready to help you")
+          //          }
+
+          //Then we should resend the queue to ALL users
+          //          for (loop <- server.users){
+          //            loop.getSocket.
+          //          }
+          server.server.getBroadcastOperations.sendEvent("queue", server.queueJSON())
+        }
       }
-      server.server.getBroadcastOperations.sendEvent("queue", server.queueJSON())
     }
   }
 }
