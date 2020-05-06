3d2739d5deb60e31625fddf4dcb33089c0dc2ec2
jing-chi <jchi3@buffalo.edu>
2020-05-06 16:22:25 -0400

    features: Distinguish between students and TAs Add authentication Only show the full queue to TAs Prevent students from entering if they are already in the queue

--- nginx/public/officeHours.js
+++ nginx/public/officeHours.js
@@ -2,10 +2,45 @@ const socket = io.connect("http://localhost:8080", {transports: ['websocket']});
 
 socket.on('queue', displayQueue);
 socket.on('message', displayMessage);
+socket.on('display', displayTwice);
 
 function displayMessage(newMessage) {
-    document.getElementById("message").innerHTML = newMessage;
+    document.getElementById("message").innerHTML = newMessage;}
+
+function displayTwice(newMessage) {
+    document.getElementById("display").innerHTML = newMessage;}
+function password(){
+let secondHTML = "<br/><h1 id=\"displayCurrency\">Enter Password</h1>";
+
+secondHTML += '<br/>';
+secondHTML += "<br/> <input type= text id=\"name\" />"
+secondHTML += "<button   onclick=\"check();\">"+ 'enter' + "</button>";
+secondHTML += '<br/>';
+document.getElementById('game').innerHTML = secondHTML;
 }
+
+function TA(){
+
+        let secondHTML = "<br/><h1 id=\"displayCurrency\">TA</h1>";
+       secondHTML += "<button style=\"width:200px;height:100px;font-size:30px\"  onclick=\"readyToHelp();\">"+ 'readyToHelp' + "</button>";
+        secondHTML += '<br/>';
+         secondHTML += "<br/> <h1 id=\"message\"></h1> ";
+         secondHTML += '<br/>';
+         secondHTML += "<br/> <h9 id=\"queue\"></h9> ";
+                  secondHTML += '<br/>';
+        document.getElementById('game').innerHTML = secondHTML;
+
+}
+
+function check(){
+let name = document.getElementById("name").value;
+         if (name == "404"){
+         TA();
+        socket.emit("student history");
+         }
+         document.getElementById("name").value = "";
+}
+
 
 function displayQueue(queueJSON) {
     const queue = JSON.parse(queueJSON);
@@ -17,11 +52,11 @@ function displayQueue(queueJSON) {
 }
 
 
-function enterQueue() {
-    let name = document.getElementById("name").value;
-    socket.emit("enter_queue", name);
-    document.getElementById("name").value = "";
-}
+ function enterQueue() {
+         let name = document.getElementById("name").value;
+         socket.emit("enter_queue", name);
+         document.getElementById("name").value = "";
+     }
 
 function readyToHelp() {
     socket.emit("ready_for_student");
