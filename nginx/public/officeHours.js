6e6117d482c229c93d3bf687c661917ff016e498
ahanaf9521 <ahanafah@buffalo.edu>
2020-05-07 18:13:04 -0400

    So I created a new button, which has a space to write down the summary of what type of problem the student is having. And the server takes the message type and send a message that the username and problem is received and should wait for a TA to get help!!!

--- nginx/public/officeHours.js
+++ nginx/public/officeHours.js
@@ -2,7 +2,6 @@ const socket = io.connect("http://localhost:8080", {transports: ['websocket']});
 
 socket.on('queue', displayQueue);
 socket.on('message', displayMessage);
-socket.on('WriteDownASummary',displayMessage);
 function displayMessage(newMessage) {
     document.getElementById("message").innerHTML = newMessage;
 }
