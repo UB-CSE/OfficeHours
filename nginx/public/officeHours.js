23fd8b438934131402a53dd153e6c878da6fa2ce
Alex Lee <dlee55@buffalo.edu>
2020-05-09 16:55:22 -0400

    separated TA and students

--- nginx/public/officeHours.js
+++ nginx/public/officeHours.js
@@ -2,6 +2,11 @@ const socket = io.connect("http://localhost:8080", {transports: ['websocket']});
 
 socket.on('queue', displayQueue);
 socket.on('message', displayMessage);
+socket.on('ta', displayTaHelp);
+
+function displayTaHelp() {
+    document.getElementById("board").innerHTML = "<button onclick='readyToHelp();'>TA Ready to Help</button>";
+}
 
 function displayMessage(newMessage) {
     document.getElementById("message").innerHTML = newMessage;
--- src/main/scala/model/OfficeHoursServer.scala
+++ src/main/scala/model/OfficeHoursServer.scala
@@ -5,7 +5,6 @@ import com.corundumstudio.socketio.{AckRequest, Configuration, SocketIOClient, S
 import model.database.{Database, DatabaseAPI, TestingDatabase}
 import play.api.libs.json.{JsValue, Json}
 
-
 class OfficeHoursServer() {
 
   val database: DatabaseAPI = if(Configuration.DEV_MODE){
@@ -16,6 +15,8 @@ class OfficeHoursServer() {
 
   var usernameToSocket: Map[String, SocketIOClient] = Map()
   var socketToUsername: Map[SocketIOClient, String] = Map()
+  var taList: List[String] = List("Alex", "Lee") //If registered TA, it will show a different pathway.
+  var inFrontOfU: Map[String, Int] = Map() //Tells how many student is in front of you
 
   val config: Configuration = new Configuration {
     setHostname("0.0.0.0")
@@ -60,10 +61,23 @@ class DisconnectionListener(server: OfficeHoursServer) extends DisconnectListene
 
 class EnterQueueListener(server: OfficeHoursServer) extends DataListener[String] {
   override def onData(socket: SocketIOClient, username: String, ackRequest: AckRequest): Unit = {
-    server.database.addStudentToQueue(StudentInQueue(username, System.nanoTime()))
     server.socketToUsername += (socket -> username)
     server.usernameToSocket += (username -> socket)
-    server.server.getBroadcastOperations.sendEvent("queue", server.queueJSON())
+    if (!server.taList.contains(username)) {
+      server.database.addStudentToQueue(StudentInQueue(username, System.nanoTime()))
+      if (server.database.getQueue.length == 1) {
+        server.usernameToSocket(username).sendEvent("message", "You are first in line")
+      } else {
+        server.usernameToSocket(username).sendEvent("message", (server.database.getQueue.length - 1).toString +
+          " students are waiting in front of you (Estimate waiting: " + (2 * (server.database.getQueue.length - 1)).toString + ")")
+        server.inFrontOfU += (username -> (server.database.getQueue.length - 1))
+      }
+    } else {
+      socket.sendEvent("ta")
+    }
+    for (ta <- server.taList) {
+      server.usernameToSocket(ta).sendEvent("queue", server.queueJSON())
+    }
   }
 }
 
@@ -74,11 +88,23 @@ class ReadyForStudentListener(server: OfficeHoursServer) extends DataListener[No
     if(queue.nonEmpty){
       val studentToHelp = queue.head
       server.database.removeStudentFromQueue(studentToHelp.username)
+      for (student <- server.inFrontOfU.keys) {
+        server.inFrontOfU += (student -> (server.inFrontOfU(student) - 1))
+        if (server.inFrontOfU(student) == 0) {
+          server.usernameToSocket(student).sendEvent("message", "You are up next")
+        } else {
+          server.usernameToSocket(student).sendEvent("message", server.inFrontOfU(student).toString +
+            " students are waiting in front of you (Estimate waiting: " + (2 * server.inFrontOfU(student)).toString + ")")
+        }
+      }
       socket.sendEvent("message", "You are now helping " + studentToHelp.username)
       if(server.usernameToSocket.contains(studentToHelp.username)){
         server.usernameToSocket(studentToHelp.username).sendEvent("message", "A TA is ready to help you")
+        server.inFrontOfU -= studentToHelp.username
       }
-      server.server.getBroadcastOperations.sendEvent("queue", server.queueJSON())
+      for (ta <- server.taList) {
+        server.usernameToSocket(ta).sendEvent("queue", server.queueJSON())
+      }
     }
   }
 }
