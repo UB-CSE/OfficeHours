4ef356233963c0bd186ef028bbd61be1d3a8f17c
mujah <Mazin894>
2020-05-10 23:57:35 -0400

    when a student enters his name he will get his spot in line and when the spot == 0, the student receive  a message letting them know a Ta is ready to meet them.

--- nginx/public/officeHours.js
+++ nginx/public/officeHours.js
@@ -1,7 +1,14 @@
 const socket = io.connect("http://localhost:8080", {transports: ['websocket']});
 
-socket.on('queue', displayQueue);
+//socket.on('queue', displayQueue);
 socket.on('message', displayMessage);
+socket.on('new_place', function (place) {
+    let placeInline = "you are number " + place + " in line";
+    if (Number(place) == 0){
+    placeInline = "Ta is ready to meet you!"
+    socket.emit("leave_queue")
+     }
+      document.getElementById("place").innerHTML = placeInline ;});
 
 function displayMessage(newMessage) {
     document.getElementById("message").innerHTML = newMessage;
@@ -20,6 +27,7 @@ function displayQueue(queueJSON) {
 function enterQueue() {
     let name = document.getElementById("name").value;
     socket.emit("enter_queue", name);
+    socket.emit("place", name);
     document.getElementById("name").value = "";
 }
 
