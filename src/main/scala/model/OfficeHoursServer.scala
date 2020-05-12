086d7899f1db94e27d3c26f9eeb63dc20e0c692c
alexander <41758627+alexanderl19@users.noreply.github.com>
2020-05-11 20:45:40 -0400

    Changed nanoTime to currentTimeMillis

--- src/main/scala/model/OfficeHoursServer.scala
+++ src/main/scala/model/OfficeHoursServer.scala
@@ -60,7 +60,7 @@ class DisconnectionListener(server: OfficeHoursServer) extends DisconnectListene
 
 class EnterQueueListener(server: OfficeHoursServer) extends DataListener[String] {
   override def onData(socket: SocketIOClient, username: String, ackRequest: AckRequest): Unit = {
-    server.database.addStudentToQueue(StudentInQueue(username, System.nanoTime()))
+    server.database.addStudentToQueue(StudentInQueue(username, System.currentTimeMillis()))
     server.socketToUsername += (socket -> username)
     server.usernameToSocket += (username -> socket)
     server.server.getBroadcastOperations.sendEvent("queue", server.queueJSON())
