dbc2cceb93d05b310bee31c9b46effe25b932a6b
Alexander <alex@alex.media>
2020-05-11 23:47:56 -0400

    Keybinds + Notifications + TA Button

--- nginx/public/officeHours.js
+++ nginx/public/officeHours.js
@@ -1,10 +1,100 @@
 const socket = io.connect("http://localhost:8080", {transports: ['websocket']});
-const input = document.getElementById("name-input");
+//Elements related to submitting name to queue
+const form = document.getElementById("name");
+const formFailBorder = document.getElementById("name-fail-border");
+const formContinueText = document.getElementById("continue-text");
+const formContent = document.getElementById("name-input");
+//Used to keep track of if the (Not Initialized) tag is removed
+const queueHeader = document.getElementById("queue-header-init");
+let queueInit = false;
+
+socket.on("connect", () => {
+    document.getElementById("server-online").src = "./assets/arrow-green.svg";
+});
+
+//Handle the user attempting to join the queue
+function handleSubmit(nameElement) {
+    try {
+        enterQueue(nameElement.value);
+        nameElement.value = "";
+    } catch (e) {
+        //If an error occurs, the border of the input field will turn red for one second
+        formFailBorder.style.opacity = "1";
+        setTimeout(() => {
+            formFailBorder.style.opacity = "0";
+        }, 1000);
+    }
+}
+
+//For [enter] key bind
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    handleSubmit(formContent);
+});
+
+//For continue text
+formContinueText.addEventListener("click", () => {
+    handleSubmit(formContent);
+});
+
+const taAcceptButton = document.getElementById("ta-accept");
+const taAcceptModal = document.getElementById("confirm-modal");
+const taAcceptNonModalKeys = document.getElementById("non-confirm-modal");
+const taAcceptModalClose = document.getElementById("confirm-modal-close");
+let modalOpen = false;
+
+const openModal = () => {
+    //Makes modal appear and hides the [ctrl] and +
+    modalOpen = true;
+    taAcceptModal.style.display = "block";
+    taAcceptNonModalKeys.style.display = "none";
+}
+
+const closeModal = () => {
+    //Opposite of openModal()
+    modalOpen = false;
+    taAcceptModal.style.display = "none";
+    taAcceptNonModalKeys.style.display = "";
+}
+
+//Set the modal to display: none when the [x] is clicked
+taAcceptModalClose.addEventListener("click", () => {
+    closeModal();
+});
+
+//Make the modal appear when a user clicks the ta accept button
+taAcceptButton.addEventListener("click", () => {
+    if (modalOpen) {
+        //Send request if modal is already open
+        readyToHelp();
+        closeModal();
+    } else {
+        //Open modal otherwise
+        openModal();
+    }
+});
+
+document.addEventListener("keyup", (e) => {
+    if (modalOpen && e.code === "Enter") {
+        //Sends request if modal is open and user presses [enter]
+        readyToHelp();
+        closeModal();
+    } else if (e.code === "Enter" && e.shiftKey && e.ctrlKey) {
+        //Skips the modal if [shift] is held in addition to [ctrl] and [enter]
+        readyToHelp();
+    } else if (e.code === "Enter" && e.ctrlKey) {
+        //Opens modal
+        openModal();
+    }
+});
 
 socket.on('queue', displayQueue);
-socket.on('message', displayMessage);
 
 function displayQueue(queueJSON) {
+    if (!queueInit) {
+        queueHeader.style.display = "none";
+        queueInit = true;
+    }
     //Parses the text into a JSON
     const queue = JSON.parse(queueJSON);
     //Get the old queue that is to be replaced
@@ -41,7 +131,7 @@ function displayQueue(queueJSON) {
         timeSpan.classList.add("time");
         //Convert the unix time into a Date object
         const timestamp = new Date(queue[i].timestamp);
-        timeSpan.innerText = `${timestamp.getHours()}:${timestamp.getSeconds() < 10 ? "0" : ""}${timestamp.getSeconds()}`;
+        timeSpan.innerText = `${timestamp.getHours()}:${timestamp.getMinutes() < 10 ? "0" : ""}${timestamp.getMinutes()}`;
 
         //Append all three elements to the parent div
         infoDiv.appendChild(nameSpan);
@@ -68,14 +158,22 @@ function displayQueue(queueJSON) {
     oldQueue.parentNode.replaceChild(newQueue, oldQueue);
 }
 
+socket.on('message', displayMessage);
+
 function displayMessage(newMessage) {
-    document.getElementById("message").innerHTML = newMessage;
+    console.log(newMessage);
+    document.getElementById("message-received").src = "./assets/arrow-green.svg";
+    document.getElementById("message-received").style.transform = "rotate(270deg)";
+    document.getElementById("message").style.display = "inline-block";
+    document.getElementById("message").innerText = newMessage;
 }
 
-function enterQueue() {
-    let name = document.getElementById("name").value;
-    socket.emit("enter_queue", name);
-    document.getElementById("name").value = "";
+function enterQueue(name) {
+    if (name) {
+        socket.emit("enter_queue", name);
+    } else {
+        throw new Error("You must enter a name to join the queue.")
+    }
 }
 
 function readyToHelp() {
