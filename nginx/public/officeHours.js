70fc6a112aa4240f3ab0c34f64cec196d26cc9d8
ajdonofr <70998110+ajdonofr@users.noreply.github.com>
2020-12-08 14:35:20 -0500

    *Various cosmetic changes, including a background image, a header bar, and a new font. *Reformatted timestamp to be more human-readable *Added colors to highlight student and TA names *Added a lose sign-in system that only allows one TA per session; authentication not required. *Messages displayed to all clients when a TA starts their session, when a TA is helping someone, when a TA is helping your client specifically, and when a TA has ended their session. *Being signed in as a TA gives access to the next button (ie only TAs can control the queue) and also the ability to sign out. A new TA can only sign in and begin a session once the previous TA has signed out and the position is vacated.

--- nginx/public/officeHours.js
+++ nginx/public/officeHours.js
@@ -1,17 +1,32 @@
 const socket = io.connect("http://localhost:8080", {transports: ['websocket']});
 
+socket.on('start', displayTA)
 socket.on('queue', displayQueue);
 socket.on('message', displayMessage);
+socket.on('tooManyTAs', displayError)
+socket.on('makeControlsAppear', controlButtonAppear)
 
 function displayMessage(newMessage) {
     document.getElementById("message").innerHTML = newMessage;
+}
+
+function displayTA(TA_name) {
+    let formattedQueue = "";
+    let TAcolor = "<span style=\"color:#005BBB;\">" + TA_name + "</span>";
+    formattedQueue += TAcolor
+    formattedQueue += " has started their session!"
+    document.getElementById("message").innerHTML = formattedQueue;
 }
 
 function displayQueue(queueJSON) {
     const queue = JSON.parse(queueJSON);
     let formattedQueue = "";
     for (const student of queue) {
-        formattedQueue += student['username'] + " has been waiting since " + student['timestamp'] + "<br/>"
+        let studentColor = "<span style=\"color:#005BBB;\">" + student['username'] + "</span>";
+        formattedQueue += studentColor
+        formattedQueue += " has been waiting since "
+        let timeColor = "<span style=\"color:#00b5c8;\">" + student['timestamp'] + "</span>";
+        formattedQueue += timeColor + "<br/>"
     }
     document.getElementById("queue").innerHTML = formattedQueue;
 }
@@ -25,4 +40,25 @@ function enterQueue() {
 
 function readyToHelp() {
     socket.emit("ready_for_student");
+}
+
+function enterTA(button) {
+    let name = document.getElementById("name").value;
+    socket.emit("check_TA", name);
+    document.getElementById("name").value = "";
+}
+
+function controlButtonAppear(){
+    let button1 = document.getElementById("nextButton")
+    let button2 = document.getElementById("signoutButton")
+    button1.disabled = false;
+    button2.disabled = false;
+}
+
+function displayError(){
+    alert("Only one TA per session")
+}
+
+function signOut() {
+    socket.emit("TA_sign_out")
 }
