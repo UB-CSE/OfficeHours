9f71cc05ca1bbbde7990bb6bffb7b6784023e0ad
githuberuser <victorbeiyong@gmail.com>
2020-12-12 16:51:13 -0500

    If this username has been taken then show a reminder and avoid this operation.

--- src/main/scala/model/OfficeHoursServer.scala
+++ src/main/scala/model/OfficeHoursServer.scala
@@ -60,10 +60,15 @@ class DisconnectionListener(server: OfficeHoursServer) extends DisconnectListene
 
 class EnterQueueListener(server: OfficeHoursServer) extends DataListener[String] {
   override def onData(socket: SocketIOClient, username: String, ackRequest: AckRequest): Unit = {
-    server.database.addStudentToQueue(StudentInQueue(username, System.nanoTime()))
-    server.socketToUsername += (socket -> username)
-    server.usernameToSocket += (username -> socket)
-    server.server.getBroadcastOperations.sendEvent("queue", server.queueJSON())
+    var usernameList=server.usernameToSocket.keySet
+    if (usernameList.contains(username)) {
+      server.usernameToSocket(username).sendEvent("message","This username has been taken, plz change a username!!  ")
+    }else {
+      server.database.addStudentToQueue(StudentInQueue(username, System.nanoTime()))
+      server.socketToUsername += (socket -> username)
+      server.usernameToSocket += (username -> socket)
+      server.server.getBroadcastOperations.sendEvent("queue", server.queueJSON())
+    }
   }
 }
 
